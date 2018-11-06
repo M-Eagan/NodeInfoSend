c023c7dbf56d1bf52b71149decfa697fe1fd932c
Megan Eagan <Megan.Eagan@gmail.com>
2018-11-05 21:06:04 -0500

    added movies

--- server.js
+++ server.js
@@ -11,3 +11,22 @@ response.end();
 
 
 //Megans-MacBook-Pro:NodeInfoSend meganeagan$ node server
+// Megans-MacBook-Pro:NodeInfoSend meganeagan$ npm init
+// This utility will walk you through creating a package.json file.
+// It only covers the most common items, and tries to guess sensible defaults.
+
+// See `npm help json` for definitive documentation on these fields
+// and exactly what they do.
+ 
+// Use `npm install <pkg>` afterwards to install a package and
+// save it as a dependency in the package.json file.
+
+// Press ^C at any time to quit.
+// package name: (nodeinfosend)
+// Megans-MacBook-Pro:NodeInfoSend meganeagan$ npm install --
+// npm notice created a lockfile as package-lock.json. You should commit this file.
+// npm WARN nodeinfosend@1.0.0 No description
+
+// up to date in 0.66s
+
+//Megans-MacBook-Pro:NodeInfoSend meganeagan$ npm install --save-dev nodemon
